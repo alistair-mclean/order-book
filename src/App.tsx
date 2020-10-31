8a87f8631b182fb70c2e9d28ff397d4871f2b165
Alistair McLean <alistair@Alistairs-MacBook-Pro.local>
2020-10-31 16:31:24 -0600

    Created a basic test for getting order books from Bittrex.
    - Spent this whole time fighting a CORS issue (been working in Python
      and Golang for the last while). Wound up adding a proxy to the
      package.json. Not sure if this is a good solution for production.
    
    - Created a basic test method for getting order books from Bittrex.
    
    - Added a button to trigger the request.

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,43 @@
 import React from 'react';
 import logo from './logo.svg';
+import axios from 'axios';
+import { AxiosRequestConfig, AxiosResponse } from 'axios';
 import './App.css';
+
+// const ENDPOINT = "https://api.bittrex.com/api/v1.1";
+
+const handleResponse = (response: AxiosResponse) => {
+  console.log(response.data);
+  console.log(response.status);
+  console.log(response.statusText);
+  console.log(response.headers);
+  console.log(response.config);
+};
+
+function getTest() : void {
+  console.log("Get test fired!!");
+  const url = "/public/getorderbook"
+  let params : AxiosRequestConfig = {
+    headers: {
+      crossdomain: true,
+    },
+    params:  {
+      market: "BTC-ETH",
+      type: "both"
+    }
+  };
+  axios.get(url, params)
+  .then(data => handleResponse(data))
+  .catch(err => console.error(err));
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          <button onClick={getTest}>Get order book</button>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
